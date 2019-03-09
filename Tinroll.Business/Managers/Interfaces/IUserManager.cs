b8a4f52d680798ecb4ee1dd04633b642b9015f29
MorganW09 <morganwkenyon@gmail.com>
2019-03-08 20:15:35 -0600

    Cleaning up merge conflicts

--- Tinroll.Business/Managers/Interfaces/IUserManager.cs
+++ Tinroll.Business/Managers/Interfaces/IUserManager.cs
@@ -6,8 +6,9 @@ using Tinroll.Model.Dto.Entity;
 
 namespace Tinroll.Business.Managers.Interfaces {
     public interface IUserManager {
-        Task<IEnumerable<UserDto>> GetAllUsersAsync
         Task CreateUserAsync(UserDto userDto);
+        Task<IEnumerable<UserDto>> GetAllUsersAsync();
+        Task<UserDto> GetUserAsync(Guid userId);
         Task UpdateUserAsync(UserDto userDto);
     }
 }
