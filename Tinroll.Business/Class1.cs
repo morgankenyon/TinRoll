5bce7ae64181cddac7b231fcf3d4d4fb4a5506f7
Morgan Kenyon <mkenyon@fileandserve.com>
2019-05-18 20:53:26 -0500

    Starting over.

--- Tinroll.Business/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace tinroll.business
-{
-    public class Class1
-    {
-    }
-}
--- Tinroll.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace tinroll.service
-{
-    public class Class1
-    {
-    }
-}
