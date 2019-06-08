6584e78b6f2e239682e8c69a756534d14ecb4799
Morgan Kenyon <morganwkenyon@gmail.com>
2019-06-08 16:01:28 -0500

    Initial work on getting the UI setup. WIP.

---
+++ src/TinRoll.Shared/QuestionDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinRoll.Shared
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int UserId { get; set; }
+        public UserDto User { get; set; }
+    }
+}
---
+++ src/TinRoll.Shared/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinRoll.Shared
+{
+    public class UserDto
+    { 
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public List<QuestionDto> Questions { get; set; }
+    }
+}
