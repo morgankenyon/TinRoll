50be9314a074c28a4cc1684b9068aeda8a3cabbe
Stefan Kenyon <stefan.kenyon@gmail.com>
2019-09-01 13:21:03 -0500

    setup react in ui folder (#24)
    * setup react in ui folder
    
    * Added node_modules/ to gitignore
    
    * Added node_modules/ to gitignore

---
+++ src/ui/app/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './app.css'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div id='tinApp'>
+                Tin
+            </div>
+        )
+    }
+}
+
+export default App
