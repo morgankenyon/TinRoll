f1210d3566f22febdfd1e1093b929c7b823a47b5
Morgan Kenyon <morganwkenyon@gmail.com>
2019-09-01 13:26:44 -0500

    Adding Cors support, but yet to be tested.

--- src/api/TinRoll.Api/Program.fs
+++ src/api/TinRoll.Api/Program.fs
@@ -8,22 +8,16 @@ open Microsoft.Extensions.Logging
 open Microsoft.EntityFrameworkCore
 open TinRoll.Data
 open FSharp.Control.Tasks
+open Microsoft.AspNetCore.Cors.Infrastructure
 
 
-type Message =
-    {
-        Text : string
-    }
 
-let helloWorld =
-    {Text = "Hello World"}
-
-let questionsHandler =
+let getQuestions =
     fun (next : HttpFunc) (ctx : HttpContext) ->
         let context = ctx.RequestServices.GetService(typeof<TinRollContext>) :?> TinRollContext
         QuestionRepo.getAll context |> ctx.WriteJsonAsync
 
-let addQuestionHandler = 
+let addQuestion = 
     fun (next : HttpFunc) (ctx : HttpContext) -> 
         task { 
             use context = ctx.RequestServices.GetService(typeof<TinRollContext>) :?> TinRollContext
@@ -34,24 +28,16 @@ let addQuestionHandler =
                     | Some l -> (setStatusCode 200 >=> json l) next ctx
                     | None -> (setStatusCode 400 >=> json "Label not added") next ctx
         }
-
-let getQuestions =
-    [
-        { Id = 1; Title = "Question #1"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-1.0)};
-        { Id = 2; Title = "Question #2"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-2.0)};
-        { Id = 3; Title = "Question #3"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-3.0)};
-        { Id = 4; Title = "Question #4"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-4.0)};
-        { Id = 5; Title = "Question #5"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-5.0)};
-        { Id = 6; Title = "Question #6"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-6.0)}
-    ]
     
 let webApp =
     choose [
         GET >=> choose [
-                route "/hello"       >=> json helloWorld
-                route "/questions" >=> questionsHandler ]
-        POST >=> route "/questions" >=> addQuestionHandler
+                route "/api/questions" >=> getQuestions ]
+        POST >=> route "/api/questions" >=> addQuestion
     ]
+
+let configureCors (builder : CorsPolicyBuilder) =
+    builder.WithOrigins("http://localhost:8080").AllowAnyMethod().AllowAnyHeader() |> ignore
 
 let configureApp (app : IApplicationBuilder) =
     // Add Giraffe to the ASP.NET Core pipeline
@@ -62,6 +48,7 @@ let configureServices (services : IServiceCollection) =
     services.AddDbContext<TinRollContext>
         (fun (options : DbContextOptionsBuilder) ->
             options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=TinRollDb;Trusted_Connection=True;MultipleActiveResultSets=true", fun f -> f.MigrationsAssembly("TinRoll.Migrations") |> ignore) |> ignore) |> ignore
+    services.AddCors() |> ignore
     services.AddGiraffe() |> ignore
 
 let configureLogging (builder : ILoggingBuilder) =
