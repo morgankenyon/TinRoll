74484da6e27fd4118a708a8424c753be43c96807
Morgan Kenyon <morganwkenyon@gmail.com>
2019-09-01 06:06:22 -0500

    Getting database working locally. Getting read and write to database for questions working as well.

--- src/api/TinRoll.Api/Program.fs
+++ src/api/TinRoll.Api/Program.fs
@@ -7,6 +7,7 @@ open Giraffe
 open Microsoft.Extensions.Logging
 open Microsoft.EntityFrameworkCore
 open TinRoll.Data
+open FSharp.Control.Tasks
 
 
 type Message =
@@ -21,6 +22,19 @@ let questionsHandler =
     fun (next : HttpFunc) (ctx : HttpContext) ->
         let context = ctx.RequestServices.GetService(typeof<TinRollContext>) :?> TinRollContext
         QuestionRepo.getAll context |> ctx.WriteJsonAsync
+
+let addQuestionHandler = 
+    fun (next : HttpFunc) (ctx : HttpContext) -> 
+        task { 
+            use context = ctx.RequestServices.GetService(typeof<TinRollContext>) :?> TinRollContext
+            let! question = ctx.BindJsonAsync<Question>()
+            return! QuestionRepo.addQuestionAsync context question
+                    |> Async.RunSynchronously
+                    |> function 
+                    | Some l -> (setStatusCode 200 >=> json l) next ctx
+                    | None -> (setStatusCode 400 >=> json "Label not added") next ctx
+        }
+
 let getQuestions =
     [
         { Id = 1; Title = "Question #1"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-1.0)};
@@ -30,22 +44,13 @@ let getQuestions =
         { Id = 5; Title = "Question #5"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-5.0)};
         { Id = 6; Title = "Question #6"; Content = "Can you help me figure this out?"; CreatedDate = DateTime.UtcNow.AddDays(-6.0)}
     ]
-
-// let questionsHandler = 
-//     fun (next : HttpFunc) (ctx : HttpContext) ->
-//         let context = ctx.RequestServices.GetService(tyoeof<LabelsContext>) :?> LabelsContext    
     
 let webApp =
     choose [
-        GET >=>
-            choose [
+        GET >=> choose [
                 route "/hello"       >=> json helloWorld
-                route "/questions" >=> json getQuestions
-            ]
-        // POST >=> 
-        //     choose [
-        //         route "/questions" >=> 
-        //     ]        
+                route "/questions" >=> questionsHandler ]
+        POST >=> route "/questions" >=> addQuestionHandler
     ]
 
 let configureApp (app : IApplicationBuilder) =
@@ -56,8 +61,7 @@ let configureServices (services : IServiceCollection) =
     // Add Giraffe dependencies
     services.AddDbContext<TinRollContext>
         (fun (options : DbContextOptionsBuilder) ->
-            options.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=TinRollDb;Trusted_Connection=True;MultipleActiveResultSets=true", fun f -> f.MigrationsAssembly("TinRoll.Migrations") |> ignore) |> ignore) |> ignore
-            //options.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=TinRollDb;Trusted_Connection=True;MultipleActiveResultSets=true") |> ignore) |> ignore
+            options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=TinRollDb;Trusted_Connection=True;MultipleActiveResultSets=true", fun f -> f.MigrationsAssembly("TinRoll.Migrations") |> ignore) |> ignore) |> ignore
     services.AddGiraffe() |> ignore
 
 let configureLogging (builder : ILoggingBuilder) =
