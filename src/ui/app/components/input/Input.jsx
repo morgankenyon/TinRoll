5dba158e5eedd27378615ad51ff879d6cce94bfa
Stefan <stefan.kenyon@gmail.com>
2019-09-23 22:02:27 -0500

    Added question validation

--- src/ui/app/components/input/Input.jsx
+++ src/ui/app/components/input/Input.jsx
@@ -3,62 +3,100 @@ import React from 'react'
 import './input.css'
 
 import { postObjectData } from '../../utils/apiCalls'
+import { withFormik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 
-export default class Input extends React.Component {
-    constructor(props) {
-        super(props)
+const Input = ({ errors, touched }) => (
+    <Form className='t-form'>
+        <div>
+            {touched.Title && errors.Title && <p className='tin-warning'>{errors.Title}</p>}
+            <Field type='text' name='Title' placeholder='title' className='t-form-input' />
+        </div>
+        <div>
+            {touched.Content && errors.Content && <p className='tin-warning'>{errors.Content}</p>}
+            <Field component='textarea' name='Content' placeholder='question' className='t-form-input'/>
+        </div>
+        <div id='tags' contentEditable="true" className='t-form-input' suppressContentEditableWarning={true}>Yes</div>
+        <button type='submit'>Submit</button>
+    </Form>
+)
 
-        this.state = {
-            title:'Second Question',
-            content:'We are trying to make this work',
-            tags:[]
+const CreateInput = withFormik({
+    mapPropsToValues({ Title, Content }) {
+        return {
+            Title: Title || 'A question about css',
+            Content: Content || 'How does this whole float thing work out?'
         }
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    async handleSubmit(e) {
-        e.preventDefault()
-        let data = {
-            Title:this.state.title,
-            Content:this.state.content,
-            UserId:1
-        }
-        console.log(data)
-        let status = await postObjectData('http://localhost:1076/api/questions', data)
+    },
+    validationSchema: Yup.object().shape({
+        Title: Yup.string().required().min(16).trim(),
+        Content: Yup.string().required().min(10).trim()
+    }),
+    async handleSubmit(values) {
+        values.UserId = 1
+        console.log(values)
+        let status = await postObjectData('http://localhost:1076/api/questions', values)
         console.log(status) 
     }
+})(Input)
 
-    handleChange(e) {
-        let target = e.target.name
-        this.setState({
-            [target]:e.target.value
-        })
+export default CreateInput
 
-    }
-    render() {
-        return ( 
-            <div className='tin-input'>
-                {this.props.children}
-                <form onSubmit={this.handleSubmit} className='t-form'>
-                    <input 
-                        type='text' 
-                        name='title' 
-                        placeholder='title' 
-                        className='t-form-element' 
-                        value={this.state.title} 
-                        onChange={this.handleChange} 
-                    />
-                    <textarea 
-                        name='content' 
-                        placeholder='question'  
-                        className='t-form-element' 
-                        value={this.state.content} 
-                        onChange={this.handleChange} 
-                    />
-                    <input type='submit' value='Ask'/>
-                </form>
-            </div>
-        )
-    }
-}
+// export default class Input extends React.Component {
+//     constructor(props) {
+//         super(props)
+
+//         this.state = {
+//             title:'Second Question',
+//             content:'We are trying to make this work',
+//             tags:[]
+//         }
+//         this.handleSubmit = this.handleSubmit.bind(this)
+//         this.handleChange = this.handleChange.bind(this)
+//     }
+
+//     async handleSubmit(e) {
+//         e.preventDefault()
+//         let data = {
+//             Title:this.state.title,
+//             Content:this.state.content,
+//             UserId:1
+//         }
+//         console.log(data)
+//         let status = await postObjectData('http://localhost:1076/api/questions', data)
+//         console.log(status) 
+//     }
+
+//     handleChange(e) {
+//         let target = e.target.name
+//         this.setState({
+//             [target]:e.target.value
+//         })
+
+//     }
+//     render() {
+//         return ( 
+//             <div className='tin-input'>
+//                 {this.props.children}
+//                 <form onSubmit={this.handleSubmit} className='t-form'>
+//                     <input 
+//                         type='text' 
+//                         name='title' 
+//                         placeholder='title' 
+//                         className='t-form-element' 
+//                         value={this.state.title} 
+//                         onChange={this.handleChange} 
+//                     />
+//                     <textarea 
+//                         name='content' 
+//                         placeholder='question'  
+//                         className='t-form-element' 
+//                         value={this.state.content} 
+//                         onChange={this.handleChange} 
+//                     />
+//                     <input type='submit' value='Ask'/>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
