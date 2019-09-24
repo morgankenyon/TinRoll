27273012840ba58ac919ae603c98a859282b980c
Stefan <stefan.kenyon@gmail.com>
2019-09-24 17:40:25 -0500

    Adding tag functionality pt1

--- src/ui/app/components/input/Input.jsx
+++ src/ui/app/components/input/Input.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import './input.css'
 
 import { postObjectData } from '../../utils/apiCalls'
-import { withFormik, Form, Field } from 'formik'
+import { withFormik, Form, Field, FieldArray } from 'formik'
 import * as Yup from 'yup'
 
-const Input = ({ errors, touched }) => (
+const Input = ({ errors, touched, values }) => (
     <Form className='t-form'>
         <div>
             {touched.Title && errors.Title && <p className='tin-warning'>{errors.Title}</p>}
@@ -16,27 +16,62 @@ const Input = ({ errors, touched }) => (
             {touched.Content && errors.Content && <p className='tin-warning'>{errors.Content}</p>}
             <Field component='textarea' name='Content' placeholder='question' className='t-form-input'/>
         </div>
-        <div id='tags' contentEditable="true" className='t-form-input' suppressContentEditableWarning={true}>Yes</div>
+        {/* <div id='tags' contentEditable="true" className='t-form-input' suppressContentEditableWarning={true}>Yes</div> */}
+        <div>
+            <FieldArray name='Tags' className='t-form-input' render={arrayHelpers => (
+                <div>
+                    <Field type='text' name='yes' onKeyUp={(e) => {
+                            if(e.keyCode === 13) {
+                                arrayHelpers.push(e.target.value)
+                            }
+                        }}/>
+                    {values.Tags && values.Tags.length > 0 && (
+                        values.Tags.map((Tag, index) => (
+                            <div key={index}>
+                                <Field name={`Tags.${index}` } />
+                                {/* <button
+                                    type="button"
+                                    onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                                >
+                                    -
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                                >
+                                    +
+                                </button> */}
+                            </div>
+                            ))
+                            )}
+                    
+                    
+                </div>
+            )} />
+        </div>
         <button type='submit'>Submit</button>
     </Form>
 )
 
 const CreateInput = withFormik({
-    mapPropsToValues({ Title, Content }) {
+    mapPropsToValues({ Title, Content, Tags, yes }) {
         return {
             Title: Title || 'A question about css',
-            Content: Content || 'How does this whole float thing work out?'
+            Content: Content || 'How does this whole float thing work out?',
+            Tags: Tags || [''],
+            yes: yes || ''
         }
     },
     validationSchema: Yup.object().shape({
         Title: Yup.string().required().min(16).trim(),
-        Content: Yup.string().required().min(10).trim()
+        Content: Yup.string().required().min(10).trim(),
+        Tags: Yup.array().required().min(1)
     }),
     async handleSubmit(values) {
         values.UserId = 1
         console.log(values)
-        let status = await postObjectData('http://localhost:1076/api/questions', values)
-        console.log(status) 
+        // let status = await postObjectData('http://localhost:1076/api/questions', values)
+        // console.log(status) 
     }
 })(Input)
 
