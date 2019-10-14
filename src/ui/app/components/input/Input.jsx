2579ae693fd38f38cfa7d8f17d1d7c02ac2ebd9b
MorganW09 <morganwkenyon@gmail.com>
2019-10-14 15:28:33 -0500

    Sending default tags. (#36)

--- src/ui/app/components/input/Input.jsx
+++ src/ui/app/components/input/Input.jsx
@@ -14,124 +14,52 @@ const Input = ({ errors, touched, values }) => (
         </div>
         <div>
             {touched.Content && errors.Content && <p className='tin-warning'>{errors.Content}</p>}
-            <Field component='textarea' name='Content' placeholder='question' className='t-form-input'/>
+            <Field component='textarea' name='Content' placeholder='question' className='t-form-input' />
         </div>
         {/* <div id='tags' contentEditable="true" className='t-form-input' suppressContentEditableWarning={true}>Yes</div> */}
         <div>
-            <FieldArray name='Tags' className='t-form-input' render={arrayHelpers => (
+            {/* <FieldArray name='Tags' className='t-form-input' render={arrayHelpers => (
                 <div>
                     <Field type='text' name='yes' onKeyUp={(e) => {
-                            if(e.keyCode === 13) {
-                                arrayHelpers.push(e.target.value)
-                            }
-                        }}/>
+                        if (e.keyCode === 13) {
+                            arrayHelpers.push(e.target.value)
+                        }
+                    }} />
                     {values.Tags && values.Tags.length > 0 && (
                         values.Tags.map((Tag, index) => (
                             <div key={index}>
-                                <Field name={`Tags.${index}` } />
-                                {/* <button
-                                    type="button"
-                                    onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
-                                >
-                                    -
-                                </button>
-                                <button
-                                    type="button"
-                                    onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
-                                >
-                                    +
-                                </button> */}
+                                <Field name={`Tags.${index}`} />
                             </div>
-                            ))
-                            )}
-                    
-                    
+                        ))
+                    )}
+
+
                 </div>
-            )} />
+            )} /> */}
         </div>
         <button type='submit'>Submit</button>
     </Form>
 )
 
 const CreateInput = withFormik({
-    mapPropsToValues({ Title, Content, Tags, yes }) {
+    mapPropsToValues({ Title, Content, TagIds, yes }) {
         return {
             Title: Title || 'A question about css',
             Content: Content || 'How does this whole float thing work out?',
-            Tags: Tags || [''],
+            TagIds: TagIds || [1, 2, 3], //need to create tags
             yes: yes || ''
         }
     },
     validationSchema: Yup.object().shape({
         Title: Yup.string().required().min(16).trim(),
         Content: Yup.string().required().min(10).trim(),
-        Tags: Yup.array().required().min(1)
+        TagIds: Yup.array().required().min(1)
     }),
     async handleSubmit(values) {
         values.UserId = 1
         console.log(values)
-        // let status = await postObjectData('http://localhost:1076/api/questions', values)
-        // console.log(status) 
+        let status = await postObjectData('http://localhost:1076/api/questions', values)
     }
 })(Input)
 
 export default CreateInput
-
-// export default class Input extends React.Component {
-//     constructor(props) {
-//         super(props)
-
-//         this.state = {
-//             title:'Second Question',
-//             content:'We are trying to make this work',
-//             tags:[]
-//         }
-//         this.handleSubmit = this.handleSubmit.bind(this)
-//         this.handleChange = this.handleChange.bind(this)
-//     }
-
-//     async handleSubmit(e) {
-//         e.preventDefault()
-//         let data = {
-//             Title:this.state.title,
-//             Content:this.state.content,
-//             UserId:1
-//         }
-//         console.log(data)
-//         let status = await postObjectData('http://localhost:1076/api/questions', data)
-//         console.log(status) 
-//     }
-
-//     handleChange(e) {
-//         let target = e.target.name
-//         this.setState({
-//             [target]:e.target.value
-//         })
-
-//     }
-//     render() {
-//         return ( 
-//             <div className='tin-input'>
-//                 {this.props.children}
-//                 <form onSubmit={this.handleSubmit} className='t-form'>
-//                     <input 
-//                         type='text' 
-//                         name='title' 
-//                         placeholder='title' 
-//                         className='t-form-element' 
-//                         value={this.state.title} 
-//                         onChange={this.handleChange} 
-//                     />
-//                     <textarea 
-//                         name='content' 
-//                         placeholder='question'  
-//                         className='t-form-element' 
-//                         value={this.state.content} 
-//                         onChange={this.handleChange} 
-//                     />
-//                     <input type='submit' value='Ask'/>
-//                 </form>
-//             </div>
-//         )
-//     }
-// }
