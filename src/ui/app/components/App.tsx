965ad7ba4c7a1b1008ca95c43b44b21ef0d7b091
Morgan Kenyon <morganwkenyon@gmail.com>
2019-10-15 10:17:28 -0500

    Moving App to tsx file, adding a container to wrap whole site in.

--- src/ui/app/components/App.tsx
+++ src/ui/app/components/App.tsx
@@ -6,44 +6,31 @@ import Question from './question/Question'
 import SingleQuestion from './question/singleQuestion/SingleQuestion'
 import Profile from './profile/Profile'
 import Footer from './footer/Footer'
+import Container from '@material-ui/core/Container';
 
 import './app.css'
 
 import { HashRouter as Router, Route } from 'react-router-dom'
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-
-    }
-
-    componentDidMount() {
-        console.log('mount app')
-    }
-
-    render() {
-        return (
+const App: React.FC = () => {
+    return (
+        <Container maxWidth="lg">
             <Router>
                 <div className='tin-app'>
-
                     <Nav />
 
                     <div className='tin-content'>
-
                         <Route exact path='/' component={Landing} />
                         <Route exact path='/question' component={Question} />
-                        <Route path='/question/:id' component={SingleQuestion} />
+                        <Route path='/questiono/:id' component={SingleQuestion} />
                         <Route path='/profile' component={Profile} />
-
                     </div>
 
                     <Footer />
-
                 </div>
             </Router>
-
-        )
-    }
+        </Container>
+    );
 }
 
-export default App
+export default App;
